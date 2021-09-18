429b39bad1ba32da38f00d8f73c60a1418e0c7fc
Taylor Galloway <gallowaytaylor@outlook.com>
2021-09-18 16:00:40 -0500

    Add sample data

---
+++ src/sample-data.js
@@ -0,0 +1,88 @@
+export default sampleData = [
+    {
+        "copyright": "AndrewStruder",
+        "date": "2017-09-06",
+        "explanation": "What should you do if your rock climbing picture is photobombed by a total eclipse of the Sun?  Rejoice -- because your planning paid off. After months of considering different venues, and a week of scouting different locations in Oregon's Smith Rock State Park, a group of photographers and rock climbers led by Andrew Struder, Ted Hesser, Martina Tibell, and Michael Shainblum settled on picturesque 100-meter tall Monkey Face tower as the dramatic foreground for their images of the pending total solar eclipse.  Tension mounted as the eclipse time approached, planned juxtapositions were scrutinized, and the placement of rock climber Tommy Smith was adjusted. Right on schedule, though, the Moon moved in front of the Sun, and Smith moved in front of the Moon, just as planned. The solar eclipse image displayed here actually shows a diamond ring, an eclipse phase when a bit of the distant Sun is still visible beyond the Moon's surface.   Open Science: Browse 1,500+ codes in the Astrophysics Source Code Library",
+        "hdurl": "https://apod.nasa.gov/apod/image/1709/EclipseClimber_Struder_3664.jpg",
+        "media_type": "image",
+        "service_version": "v1",
+        "title": "The Climber and the Eclipse",
+        "url": "https://apod.nasa.gov/apod/image/1709/EclipseClimber_Struder_1080.jpg"
+    },
+    {
+        "copyright": "Jeff Husted",
+        "date": "2013-12-24",
+        "explanation": "Blown by fast winds from a hot, massive star, this cosmic bubble is huge. Cataloged as Sharpless 2-308 it lies some 5,200 light-years away toward the constellation of the Big Dog (Canis Major) and covers slightly more of the sky than a Full Moon. That corresponds to a diameter of 60 light-years at its estimated distance. The massive star that created the bubble, a Wolf-Rayet star, is the bright one near the center of the nebula. Wolf-Rayet stars have over 20 times the mass of the Sun and are thought to be in a brief, pre-supernova phase of massive star evolution. Fast winds from this Wolf-Rayet star create the bubble-shaped nebula as they sweep up slower moving material from an earlier phase of evolution. The windblown nebula has an age of about 70,000 years. Relatively faint emission captured in the expansive image is dominated by the glow of ionized oxygen atoms mapped to violet hues.   Budget Universe: Free APOD 2014 Calendar in PDF format",
+        "hdurl": "https://apod.nasa.gov/apod/image/1312/sharpless308_husted_1658.jpg",
+        "media_type": "image",
+        "service_version": "v1",
+        "title": "Sharpless 308: Star Bubble",
+        "url": "https://apod.nasa.gov/apod/image/1312/sharpless308_husted_960.jpg"
+    },
+    {
+        "copyright": "2MASST. H. Jarrett",
+        "date": "2003-09-17",
+        "explanation": "Are the nearest galaxies distributed randomly? A plot of over one million of the brightest \"extended sources\" detected by the Two Micron All Sky Survey (2MASS) shows that they are not.  The vast majority of these infrared extended sources are galaxies.  Visible above is an incredible tapestry of structure that provides limits on how the universe formed and evolved.  Many galaxies are gravitationally bound together to form clusters, which themselves are loosely bound into superclusters, which in turn are sometimes seen to align over even larger scale structures.  In contrast, very bright stars inside our own Milky Way Galaxy cause the vertical blue sash.",
+        "hdurl": "https://apod.nasa.gov/apod/image/0309/galaxysky_2mass_big.jpg",
+        "media_type": "image",
+        "service_version": "v1",
+        "title": "The 2MASS Galaxy Sky",
+        "url": "https://apod.nasa.gov/apod/image/0309/galaxysky_2mass.jpg"
+    },
+    {
+        "date": "2005-10-01",
+        "explanation": "When morning twilight came to the Paranal Observatory in Chile, astronomers Mark Neeser and Peter Barthel interrupted their search for faint quasars, billions of light-years away. And just for a moment, they used Very Large Telescopes at the European Southern Observatory to appreciate the beauty of the nearby Universe. One result was this stunning view of beautiful barred spiral galaxy NGC 613, a mere 65 million light-years away in the southern constellation Sculptor. Over 100 thousand light-years across, NGC 613 seems to have more than its fair share of spiral arms laced with cosmic dust clouds and bright star forming regions near the ends of a dominant central bar. Radio emission indicates the presence of a massive black hole at the center of NGC 613.",
+        "hdurl": "https://apod.nasa.gov/apod/image/0510/ngc613_eso_full.jpg",
+        "media_type": "image",
+        "service_version": "v1",
+        "title": "NGC 613: Spiral of Dust and Stars",
+        "url": "https://apod.nasa.gov/apod/image/0510/ngc613_eso_c33.jpg"
+    },
+    {
+        "copyright": "Marcin Zając",
+        "date": "2020-08-13",
+        "explanation": "What planets are those behind that unusual rock spire? Saturn (lower left) and Jupiter.  This month, after sunset, the bright planetary duo are quite prominent toward the southeast.  Now your view of our Solar System's largest planets might not include a picturesque hoodoo in the foreground, nor the spectacular central band of our Milky Way Galaxy across the background, but should be quite eye-catching anyway.  The featured image is a composite of consecutive foreground and background exposures all taken in late May with the same camera and from the same location -- the badlands of the  Ah-Shi-Sle-Pah Wilderness in the San Juan Basin in New Mexico, USA.  The rock spire, informally dubbed 'Alien Throne', stands about 3 meters tall. Saturn and Jupiter will remain visible together after sunset for several months.",
+        "hdurl": "https://apod.nasa.gov/apod/image/2008/AlienThrone_Zajac_3807.jpg",
+        "media_type": "image",
+        "service_version": "v1",
+        "title": "Jupiter and Saturn Rising Beyond Alien Throne Rock",
+        "url": "https://apod.nasa.gov/apod/image/2008/AlienThrone_Zajac_960.jpg"
+    },
+    {
+        "date": "2000-01-24",
+        "explanation": "In 1787, astronomer William Herschel discovered  NGC 2392.  From the ground, NGC 2392 resembles a person's head surrounded by a parka hood.  In 2000, just after being fixed, the Hubble Space Telescope imaged  NGC 2392.   From space, the nebula displays gas clouds so complex they are not fully understood.   NGC 2392 is clearly a planetary nebula, and the gas seen above composed the outer layers of a Sun-like star only 10,000 years ago.  The inner filaments visible above are being ejected by strong wind of particles from the central star.  The outer disk contains unusual light-year long orange filaments.  NGC 2392 lies about 5000 light-years away and is visible with a small telescope in the constellation of Gemini.",
+        "hdurl": "https://apod.nasa.gov/apod/image/0001/ngc2392_hst_big.jpg",
+        "media_type": "image",
+        "service_version": "v1",
+        "title": "NGC 2392 from the Newly Fixed Hubble",
+        "url": "https://apod.nasa.gov/apod/image/0001/ngc2392_hst.jpg"
+    },
+    {
+        "date": "2000-10-23",
+        "explanation": "Our Milky Way Galaxy is not alone.  It is part of a gathering of about 25 galaxies known as the Local Group.  Members include the Great Andromeda Galaxy (M31), M32, M33, the Large Magellanic Cloud, the Small Magellanic Cloud, Dwingeloo 1, several small irregular galaxies, and many dwarf elliptical and dwarf spheroidal galaxies.  Pictured on the lower left is one of the many dwarf ellipticals: NGC 205.  Like M32, NGC 205 is a companion to the large M31, and can sometimes be seen to the south of M31's center in photographs.  The above image shows NGC 205 to be unusual for an elliptical galaxy in that it contains at least two dust clouds (at 1 and 4 o'clock - they are visible but hard to spot) and signs of recent star formation.  This galaxy is sometimes known as M110, although it was actually not part of Messier's original catalog.",
+        "hdurl": "https://apod.nasa.gov/apod/image/0010/ngc205_cfht_big.jpg",
+        "media_type": "image",
+        "service_version": "v1",
+        "title": "Dwarf Elliptical Galaxy NGC 205 in the Local Group",
+        "url": "https://apod.nasa.gov/apod/image/0010/ngc205_cfht.jpg"
+    },
+    {
+        "copyright": "Colin LeonhardtBirdseye View Photography",
+        "date": "2014-09-30",
+        "explanation": "Have you ever seen an entire rainbow? From the ground, typically, only the top portion of a rainbow is visible because directions toward the ground have fewer raindrops. From the air, though, the entire 360 degree circle of a rainbow is more commonly visible. Pictured here, a full circle rainbow was captured over Cottesloe Beach near Perth, Australia last year by a helicopter flying between a setting sun and a downpour. An observer-dependent phenomenon primarily caused by the internal reflection of sunlight by raindrops, the 84-degree diameter rainbow followed the helicopter, intact, for about 5 kilometers. As a bonus, a second rainbow that was more faint and color-reversed was visible outside the first.   APOD Astrophotographers: Has your work ever been used for scientific discovery?",
+        "hdurl": "https://apod.nasa.gov/apod/image/1409/fullrainbow_leonhardt_1500.jpg",
+        "media_type": "image",
+        "service_version": "v1",
+        "title": "A Full Circle Rainbow over Australia",
+        "url": "https://apod.nasa.gov/apod/image/1409/fullrainbow_leonhardt_960.jpg"
+    },
+    {
+        "date": "2010-07-03",
+        "explanation": "A mere 50 light-years away, young star Beta Pictoris became one of the most important stars in the sky in the early 1980s. Satellite and ground-based telescopic observations revealed the presence of a surrounding outer, dusty, debris disk and an inner clear zone about the size of our solar system -- strong evidence for the formation of planets. Infrared observations from European Southern Observatory telescopes subsequently detected a source in the clear zone, now confirmed as a giant planet orbiting Beta Pic. The confirmation comes as the planet is detected at two different positions in its orbit. Designated Beta Pictoris b, the giant planet must have formed rapidly as Beta Pic itself is only 8 to 20 million years old. With an orbital period estimated between 17 and 44 years, Beta Pictoris b could lie near the orbit of Saturn if found in our solar system, making it the closest planet to its parent star directly imaged ... so far.",
+        "hdurl": "https://apod.nasa.gov/apod/image/1007/betapicb_caption.jpg",
+        "media_type": "image",
+        "service_version": "v1",
+        "title": "A Giant Planet for Beta Pic",
+        "url": "https://apod.nasa.gov/apod/image/1007/betapicb_caption800.jpg"
+    }
+]
