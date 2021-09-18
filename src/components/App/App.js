ed8af903afa4e801bdf2cf2223da84103eedb55d
Taylor Galloway <gallowaytaylor@outlook.com>
2021-09-18 15:54:36 -0500

    Add header JSX

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { NavLink } from 'react-router-dom';
 
 const App = () => {
   return (
     <main>
-
+      <header className='header'>
+        <button>Randomizer</button>
+        <h1>My Space Book</h1>
+        <NavLink>My Liked Images</NavLink>
+      </header>
+      
     </main>
   );
 }
