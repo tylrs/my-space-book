ae581b75ac509fcfd8247e5ecdc24045f5220a47
Taylor Galloway <gallowaytaylor@outlook.com>
2021-09-18 22:23:55 -0500

    Add click handler for like button

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -2,14 +2,19 @@ import React from 'react';
 import './Image.css';
 
 const Image = ({imageInfo, likeOrUnlikeImage}) => {
-    const {url, title, date} = imageInfo;
+    const {url, title, date, id} = imageInfo;
+
+    const handleClick = () => {
+        const result = likeOrUnlikeImage(id)
+        console.log(result)
+    }
 
     return (
         <article className='space-image-box'>
             <img src={url} alt={title}/>
             <p>{title}</p>
             <p>{date}</p>
-            <button>Like</button>
+            <button onClick={() => {handleClick()}}>Like</button>
         </article>
     )
 }
