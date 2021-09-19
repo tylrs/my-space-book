afaf441c23944927d8366801776c65fb51665415
Taylor Galloway <gallowaytaylor@outlook.com>
2021-09-18 21:03:40 -0500

    Set up getImages function in app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import { React, useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import Images from '../Images/Images';
 import { sampleData } from '../../sample-data';
 
 const App = () => {
+  const [images, setImages] = useState([])
+  const [error, setError] = useState('')
+
+  const getImages = async () => {
+    setError('')
+    try {
+      const imageData = await fetchImages()
+      setImages(imageData)
+    } catch(err) {
+      setError(err)
+    }
+  }
+
+  useEffect(() => {
+
+  }) 
+
   return (
     <main>
       <header className='header'>
