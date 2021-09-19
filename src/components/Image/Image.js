935579fb65a6460043f43c1a0e2296bf040131de
Taylor Galloway <gallowaytaylor@outlook.com>
2021-09-19 10:00:16 -0500

    Add like message and timer

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -3,17 +3,25 @@ import './Image.css';
 
 const Image = ({imageInfo, likeOrUnlikeImage}) => {
     const [likeMode, setLikeMode] = useState(true)
+    const [likeMessage, setLikeMessage] = useState(false)
+    const [timer, setTimer] = useState('')
 
     const {url, title, date, id} = imageInfo;
 
     const handleClick = () => {
         const result = likeOrUnlikeImage(id)
         setLikeMode(result)
+        setLikeMessage(true)
+        setTimer(setTimeout(() => {
+            setLikeMessage(false)
+        }, 3000))
+
     }
 
     return (
         <article className='space-image-box'>
             <img src={url} alt={title}/>
+            {likeMessage && <p className='like-message'>{likeMode ? 'Unliked' : 'Liked'}</p>}
             <p>{title}</p>
             <p>{date}</p>
             <button onClick={() => {handleClick()}}>{likeMode ? 'Like' : 'Unlike'}</button>
