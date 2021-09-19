0122ce6f9b6d7d331e5e5105e7488f5b0810a89e
Taylor Galloway <gallowaytaylor@outlook.com>
2021-09-18 22:17:17 -0500

    Make function dynamic for liking or unliking

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,20 +2,35 @@ import './App.css';
 import { React, useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import Images from '../Images/Images';
-import { fetchImages } from '../../utils';
-// import { sampleData } from '../../sample-data';
+import { fetchImages, cleanImages } from '../../utils';
+import { sampleData } from '../../sample-data';
 
 const App = () => {
-  const [images, setImages] = useState([])
-  const [error, setError] = useState('')
+  const [images, setImages] = useState([]);
+  const [likedImages, setLikedImages] = useState([]);
+  const [likedImageIds, setLikedImageIds] = useState({});
+  const [error, setError] = useState('');
 
   const getImages = async () => {
     setError('')
     try {
-      const imageData = await fetchImages()
-      setImages(imageData)
+      // const imageData = await fetchImages()
+      const cleanedImages = cleanImages(sampleData)
+      setImages(cleanedImages)
     } catch(err) {
       setError(err)
+    }
+  }
+
+  const likeOrUnlikeImage = (id) => {
+    if (!likedImageIds[id]) {
+      setLikedImageIds({id, ...likedImageIds})
+      const foundImage = images.find(image => image.id === id)
+      setLikedImages([foundImage, ...likedImages])
+    } else {
+      delete likedImageIds[id]
+      const filteredImages = likedImages.filter(image => image.id !== id)
+      setLikedImages([...filteredImages])
     }
   }
 
@@ -25,6 +40,7 @@ const App = () => {
 
   return (
     <main>
+      {/* {console.log(images)} */}
       <header className='header'>
         <button onClick={() => {getImages()}}>Get New Images</button>
         <h1>My Space Book</h1>
