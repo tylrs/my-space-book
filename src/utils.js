44a78da10066ce5a5c608a202d9d9c301d430fa7
Taylor Galloway <gallowaytaylor@outlook.com>
2021-09-18 21:09:21 -0500

    Add fetch apiCall

---
+++ src/utils.js
@@ -0,0 +1,13 @@
+export const fetchImages = async () => {
+    try {
+        const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=iFKelpSH9TLhm1UyPVmKoRwpzbZm9mZWe9WeDBRy&count=9')
+        if (!response.ok) {
+            throw Error("Hmm looks like those images are lost in space...try refreshing the page!");
+        } else {
+            const parsed = await response.json();
+            return parsed;
+        }
+    } catch (err) {
+        throw Error(err)
+    }
+}
