6c0cb0c95885f55d1ca660e804d50ebaaa111712
Taylor Galloway <gallowaytaylor@outlook.com>
2021-09-19 19:16:51 -0500

    Add homepage loading test

---
+++ cypress/integration/homepage_spec.js
@@ -0,0 +1,35 @@
+describe('Homepage', () => {
+    beforeEach(() => {
+        cy.intercept('GET', 'https://api.nasa.gov/planetary/apod?api_key=iFKelpSH9TLhm1UyPVmKoRwpzbZm9mZWe9WeDBRy&count=9', {
+            ok: true,
+            statusCode: 200,
+            fixture: 'sampleImages'
+        }).as('getImages')
+        .visit('http://localhost:3000/')
+    })
+
+    it('Should be able to visit the homepage and see a url, title images, and buttons', () => {
+        cy
+            .url().should('include', 'http://localhost:3000/')
+            .wait('@getImages')
+            .get('h1')
+            .contains('My Space Book')
+            .get('.get-images')
+            .contains('Get New Images')
+            .get('.liked-images-nav')
+            .contains('My Liked Images')
+            .get('.images')
+            .children()
+            .should('have.length', 9)
+            .get('.images > :nth-child(1)')
+            .contains('The Climber and the Eclipse')
+            .get('.images > :nth-child(1)')
+            .contains('9/5/2017')
+            .get('.images > :nth-child(1)')
+            .contains('Like')
+            .get('.images > :nth-child(1) > img')
+            .should('be.visible')
+            
+
+    })
+})
