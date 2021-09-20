2dbc37f9227b2c64ab9b7445a8e1501a8fa82c12
Taylor Galloway <gallowaytaylor@outlook.com>
2021-09-19 19:31:48 -0500

    Add 2nd test for retrieving new images

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -29,7 +29,29 @@ describe('Homepage', () => {
             .contains('Like')
             .get('.images > :nth-child(1) > img')
             .should('be.visible')
-            
+    })
 
+    it('Should be able to click the get new images button and see a new list of images', () => {
+        cy
+            .wait('@getImages')
+            .intercept('GET', 'https://api.nasa.gov/planetary/apod?api_key=iFKelpSH9TLhm1UyPVmKoRwpzbZm9mZWe9WeDBRy&count=9', {
+                ok: true,
+                stausCode: 200,
+                fixture: 'sampleImages2'
+            }).as('getImages2')
+            .get('.get-images')
+            .click()
+            .wait('@getImages2')
+            .get('.images')
+            .children()
+            .should('have.length', 9)
+            .get('.images > :nth-child(1)')
+            .contains('Markarian\'s Chain of Galaxies')
+            .get('.images > :nth-child(1)')
+            .contains('3/30/2019')
+            .get('.images > :nth-child(1)')
+            .contains('Like')
+            .get('.images > :nth-child(1) > img')
+            .should('be.visible')
     })
 })
